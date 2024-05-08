ff21890fbf7fe3b76427a288fe15ac18f9b0a87c
Lucas Loureiro <lucas-loureiro@outlook.com>
2024-05-08 20:34:38 -0300

    feat: add log for testing purposes

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,6 +9,9 @@ const publicRoutes = ['/login']
 
 export async function middleware(req: NextRequest) {
   const pathname = req.nextUrl.pathname
+  const nextUrl = req.nextUrl
+  console.log('nextUrl:')
+  console.log(nextUrl)
 
   if(publicRoutes.includes(pathname)) {
     return NextResponse.next()
@@ -17,7 +20,8 @@ export async function middleware(req: NextRequest) {
   const session = await validateSession()
 
   if (!session) return NextResponse.redirect(new URL('/login', req.url))
-  if (pathname === '/') return NextResponse.redirect(new URL('/painel', req.url))
+  if (pathname === '/painel') return NextResponse.redirect(new URL('/painel/associados', req.url))
+  if (pathname === '/') return NextResponse.redirect(new URL('/painel/associados', req.url))
 
   return NextResponse.next()
 }
